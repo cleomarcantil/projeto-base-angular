ca6b7d340e9ce8a3ec7e758bdbe83477de0aaca6
Cleomar <cleomarcantil@gmail.com>
2017-10-27 08:42:53 -0300

    Ajustes no layout. Criado componente  para página inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'Modelo de aplicação Angular';
+
+
+	private onActivate(component) {
+		//console.log(component);
+	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+
+const routes : Routes = [
+	{
+		path: '',
+		component: IndexComponent,
+		pathMatch: 'full',
+	},
+	// { // rota para um módulo lazy load
+	// 	path: 'testes',
+	// 	loadChildren: 'app/testes/testes.module#TestesModule',
+	// },
+];
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		IndexComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule,
+		RouterModule.forRoot(routes)
 	],
 	providers: [],
 	bootstrap: [AppComponent]
